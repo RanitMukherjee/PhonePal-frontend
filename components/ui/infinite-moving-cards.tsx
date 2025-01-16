8d8fa281c120479dfefa0949d738a923a681911f
RanitMukherjee <49058364+RanitMukherjee@users.noreply.github.com>
2025-01-17 03:06:25 +0530

    deploy

--- components/ui/infinite-moving-cards.tsx
+++ components/ui/infinite-moving-cards.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps, @typescript-eslint/no-unused-vars */
 "use client";
 
 import { cn } from "@/lib/utils";
@@ -22,29 +23,27 @@ export const InfiniteMovingCards = ({
 }) => {
   const containerRef = React.useRef<HTMLDivElement>(null);
   const scrollerRef = React.useRef<HTMLUListElement>(null);
-  const [start, setStart] = useState(false);
 
   useEffect(() => {
-    const addAnimation = () => {
-      if (containerRef.current && scrollerRef.current) {
-        const scrollerContent = Array.from(scrollerRef.current.children);
-
-        scrollerContent.forEach((item) => {
-          const duplicatedItem = item.cloneNode(true);
-          if (scrollerRef.current) {
-            scrollerRef.current.appendChild(duplicatedItem);
-          }
-        });
-
-        getDirection();
-        getSpeed();
-        setStart(true);
-      }
-    };
-
     addAnimation();
-  }, [direction, speed]); // Add dependencies if needed
+  }, []);
+  const [start, setStart] = useState(false);
+  function addAnimation() {
+    if (containerRef.current && scrollerRef.current) {
+      const scrollerContent = Array.from(scrollerRef.current.children);
 
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        if (scrollerRef.current) {
+          scrollerRef.current.appendChild(duplicatedItem);
+        }
+      });
+
+      getDirection();
+      getSpeed();
+      setStart(true);
+    }
+  }
   const getDirection = () => {
     if (containerRef.current) {
       if (direction === "left") {
@@ -60,7 +59,6 @@ export const InfiniteMovingCards = ({
       }
     }
   };
-
   const getSpeed = () => {
     if (containerRef.current) {
       if (speed === "fast") {
@@ -72,7 +70,6 @@ export const InfiniteMovingCards = ({
       }
     }
   };
-
   return (
     <div
       ref={containerRef}
@@ -89,7 +86,7 @@ export const InfiniteMovingCards = ({
           pauseOnHover && "hover:[animation-play-state:paused]"
         )}
       >
-        {items.map((item, _) => ( // Replace `idx` with `_` to indicate it's unused
+        {items.map((item, idx) => (
           <li
             className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px]"
             style={{
@@ -108,10 +105,10 @@ export const InfiniteMovingCards = ({
               </span>
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 <span className="flex flex-col gap-1">
-                  <span className="text-sm leading-[1.6] text-gray-400 font-normal">
+                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
                     {item.name}
                   </span>
-                  <span className="text-sm leading-[1.6] text-gray-400 font-normal">
+                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
                     {item.title}
                   </span>
                 </span>
